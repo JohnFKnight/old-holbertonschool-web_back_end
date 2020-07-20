8f5fcf6b1330a27d91d819763e11600b3e601b31
jfkquality <jfkquality@gmail.com>
2020-07-20 18:04:50 -0400

    Add .gitignore

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def floor(n: float ) -> int:
+def floor(n: float) -> int:
 
     """My addition function
 
--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def to_str(n: float ) -> str:
+def to_str(n: float) -> str:
 
     """My addition function
 
