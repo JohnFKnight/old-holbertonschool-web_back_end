7132d26f3a0d5e4b79fc813f5924bc36f6c30a29
jfkquality <jfkquality@gmail.com>
2020-07-21 00:08:32 -0400

    Task 11/101, Init commit.

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Make sample code annoted."""
+
+
+from typing import Any, Union, Mapping
+
+
+def safely_get_value(dct: Mapping[key: Any, Union[None]]) -> Union[Any]:
+    """Make sample code annoted Duck typing."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
