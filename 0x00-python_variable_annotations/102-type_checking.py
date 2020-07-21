1c133e2a8a2bc079dd98d79633a2e5c837f98ea8
jfkquality <jfkquality@gmail.com>
2020-07-21 03:11:58 -0400

    Task 12/102, debugging.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,6 +1,12 @@
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    zoomed_in: Tuple = [
-        item for item in lst
-        for i in range(factor)
-    ]
-    return zoomed_in
+#!/usr/bin/env python3
+
+def zoom_array(lst: Tuple, fctr: int = 2) -> Tuple:
+    john: Tuple = [item for item in lst, for i in range(fctr)]
+    return john
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
