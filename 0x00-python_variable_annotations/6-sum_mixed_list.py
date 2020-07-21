b812a3ecb45a56d8103efedef07b02c6145e6929
jfkquality <jfkquality@gmail.com>
2020-07-20 20:28:20 -0400

    Task 0: bugfix. Linter: Add doc strings above and within function.

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
+"""Sum a mixed list."""
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """Sum a mixed list of ints and floats."""
     return (sum(mxd_list))
