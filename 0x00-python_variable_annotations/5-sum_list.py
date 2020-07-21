ad8a26231b086ff354bf85f7f5dd2f7dd02e5a1c
jfkquality <jfkquality@gmail.com>
2020-07-20 20:12:37 -0400

    Task 5. Init commit.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+
+    """sum_list function
+
+    Args:
+    input_list: list of floats to sum
+
+    Returns:
+        Sum of the list of floats as float
+    """
+
+    return (sum(input_list))
