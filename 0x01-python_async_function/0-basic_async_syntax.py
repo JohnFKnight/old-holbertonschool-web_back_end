0d5c340d801592db2ef583680bdb7985666131f4
jfkquality <jfkquality@gmail.com>
2020-07-21 23:41:37 +0000

    Task 0, bugfix: Linter err: add 2nd blank line after imports.

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Basic asynch syntax."""
 
-
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """Basic async  syntax of await."""
