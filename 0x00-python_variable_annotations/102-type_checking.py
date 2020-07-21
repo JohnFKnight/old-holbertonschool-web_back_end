f0e102098b9cf76b1189b32b7808746c475ca64d
jfkquality <jfkquality@gmail.com>
2020-07-21 03:21:49 -0400

    Task 12/102, bugfix: Blind fixes.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,13 +2,16 @@
 """ Sample code. Correct and annotate."""
 
 
-def zoom_array(lst: Tuple, fctr: int = 2) -> Tuple:
+def zoom_array(lst: Tuple, fctr: int = 2) -> Tuple[float, int]:
     john: Tuple = [item for item in lst for i in range(fctr)]
     return john
 
 
 array = [12, 72, 91]
+print(array)
 
 zoom_2x = zoom_array(array)
+print(zoom_2x)
 
 zoom_3x = zoom_array(array, 3.0)
+print(zoom_3x)
