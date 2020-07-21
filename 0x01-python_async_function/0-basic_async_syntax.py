633a87b8bab9234d6e86a0f55f3600deed5d0e01
jfkquality <jfkquality@gmail.com>
2020-07-21 23:38:47 +0000

    Task 0, bugfix: Add doc strings. Add forgotten await command!

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+""" Basic asynch syntax."""
+
 
 import asyncio
 import random
 
-
 async def wait_random(max_delay: int = 10) -> float:
+    """Basic async  syntax of await."""
     delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return (delay)
