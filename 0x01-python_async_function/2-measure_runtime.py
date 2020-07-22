b2c233163a3c8acd03e23b125943f22d600ccf27
jfkquality <jfkquality@gmail.com>
2020-07-22 04:34:15 +0000

    Task 2, Init commit.

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""execute multiple coroutines at the same time with async."""
+
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """execute multiple coroutines at the same time with async."""
+
+    elaspsed: float
+    
+    s = time.perf_counter()
+    await wait_n(max_delay, n)
+    elapsed = time.perf_counter() - s
+    return (elapsed)
