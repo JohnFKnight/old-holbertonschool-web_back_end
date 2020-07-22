bf750e235f0c50f89f45fb6b980be2ad9ff5d773
jfkquality <jfkquality@gmail.com>
2020-07-22 03:44:45 +0000

    Task 1, Init commit.

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""execute multiple coroutines at the same time with async."""
+
+import asyncio
+from typing import List
+import nest_asyncio
+# nest_asyncio.apply()
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(max_delay: int, n: int) -> List[float]:
+    """execute multiple coroutines at the same time with async."""
+    list1: List[float] = []
+    dly_list: List[float] = []
+    for i in range(n):
+        # dly_list.append(asyncio.ensure_future(wait_random(max_delay)))
+        list1.append(asyncio.ensure_future(wait_random(max_delay)))
+    for j in list1:
+        dly_list.append(await j)
+
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(wait_n(max_delay, n))
+    return (dly_list)
