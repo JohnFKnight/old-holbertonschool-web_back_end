01936f57de332da056cad5fff531be642bff575b
jfkquality <jfkquality@gmail.com>
2020-07-23 23:33:49 +0000

    Task 101, refactor: Change TypeVar to T from V for checker.

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,7 +5,7 @@
 from typing import Any, Union, Mapping, TypeVar
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('V'), type(None)]) -> Union[Any, TypeVar('V')]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), type(None)]) -> Union[Any, TypeVar('T')]:
     """Make sample code annoted Duck typing."""
     if key in dct:
         return dct[key]
