747192569e85ac4995a703921d707fab282b2bf9
jfkquality <jfkquality@gmail.com>
2020-07-23 06:55:43 +0000

    Task 1, bugfix: reverse function parameter positions. Unknown til Task 2.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay: int, n: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """execute multiple coroutines at the same time with async."""
     list1: List[float] = []
     dly_list: List[float] = []
