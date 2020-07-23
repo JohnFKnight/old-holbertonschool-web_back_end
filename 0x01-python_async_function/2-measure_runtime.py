f25a3cb8aaf5dc1b9a71b37cec1600ccc9982e88
jfkquality <jfkquality@gmail.com>
2020-07-23 06:42:05 +0000

    Task 2, bugfix: go back to original code.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-""" Testing async """
+"""execute multiple coroutines at the same time with async."""
 
-
-from typing import List
-import time
 import asyncio
-import random
+import time
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int = 10) -> float:
-    """ Wait a random amount of delay and then return that number"""
-    seconds = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    return (time.perf_counter() - seconds)/n
+async def measure_time(n: int, max_delay: int) -> float:
+    """execute multiple coroutines at the same time with async."""
+
+    elaspsed: float
+
+    s = time.perf_counter()
+    await wait_n(max_delay, n)
+    elapsed = time.perf_counter() - s
+    return (elapsed)
