79bd1081bc796bd6ccbe90d33a061a7009181c19
jfkquality <jfkquality@gmail.com>
2020-07-23 03:59:56 +0000

    Task 2, bugfix: Add await before gather stmt.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,7 +11,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Parallel async comprehension."""
     s: float = time.perf_counter()
-    asyncio.gather(async_comprehension(),
+    await asyncio.gather(async_comprehension(),
                    async_comprehension(),
                    async_comprehension(),
                    async_comprehension())
