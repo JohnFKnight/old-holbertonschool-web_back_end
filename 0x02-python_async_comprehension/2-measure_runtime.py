adfec854ea109cfe74ce1c1703df3ac5ac3a65ad
jfkquality <jfkquality@gmail.com>
2020-07-23 04:24:30 +0000

    Task 1, bugfix: Use [] around gather arguments.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,8 +11,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Parallel async comprehension."""
     s: float = time.perf_counter()
-    await asyncio.gather(async_comprehension(),
+    await asyncio.gather[async_comprehension(),
                          async_comprehension(),
                          async_comprehension(),
-                         async_comprehension())
+                         async_comprehension()]
     return (time.perf_counter() - s)
