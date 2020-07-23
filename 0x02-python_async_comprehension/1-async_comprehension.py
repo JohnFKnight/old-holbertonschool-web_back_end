6216a57dd3f5100ecaf323fcdc5d98976b3b247e
jfkquality <jfkquality@gmail.com>
2020-07-23 04:13:23 +0000

    Task 1, bugfix: Correct return annotation from Generator to List.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,14 +2,13 @@
 """ asynch comprehension."""
 
 import asyncio
-import random
-from typing import Generator
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]
     """Basic async comprehension."""
     result = [i async for i in async_generator() if i < 10]
     return (result)
