603239bf70f5447d5f6b39d21e0b3720c8135620
jfkquality <jfkquality@gmail.com>
2020-07-23 03:55:14 +0000

    Task 2, bugfix: Linter errs. Line too long; trailing spaces.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,5 +11,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Parallel async comprehension."""
     s: float = time.perf_counter()
-    asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension()) 
+    asyncio.gather(async_comprehension(),
+                   async_comprehension(),
+                   async_comprehension(),
+                   async_comprehension())
     return (time.perf_counter() - s)
