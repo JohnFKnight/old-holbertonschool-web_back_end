823512b6da71c6874ae527383cc83d8b343c6694
jfkquality <jfkquality@gmail.com>
2020-07-23 04:01:12 +0000

    Task 2, bugfix: Linter err: indentation.

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,7 +12,7 @@ async def measure_runtime() -> float:
     """Parallel async comprehension."""
     s: float = time.perf_counter()
     await asyncio.gather(async_comprehension(),
-                   async_comprehension(),
-                   async_comprehension(),
-                   async_comprehension())
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
     return (time.perf_counter() - s)
