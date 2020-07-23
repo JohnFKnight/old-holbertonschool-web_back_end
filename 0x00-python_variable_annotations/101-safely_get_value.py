b503bf256d9bdcbb709fd8539cb4969d7e8bed57
jfkquality <jfkquality@gmail.com>
2020-07-23 23:32:09 +0000

    Task 101, feature: Finish task. Get proper types/annotations.

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,10 +2,10 @@
 """Make sample code annoted."""
 
 
-from typing import Any, Union, Mapping
+from typing import Any, Union, Mapping, TypeVar
 
 
-def safely_get_value(dct: [Mapping[key: Any, Union[None]]]) -> Union[Any]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('V'), type(None)]) -> Union[Any, TypeVar('V')]:
     """Make sample code annoted Duck typing."""
     if key in dct:
         return dct[key]
