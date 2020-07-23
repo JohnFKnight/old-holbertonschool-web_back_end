f46ae5b89bb43bc570abd1c16f6aca4bffddc3a8
jfkquality <jfkquality@gmail.com>
2020-07-23 04:19:48 +0000

    Task 1, bugfix: Put colon back after func def.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,7 @@ from typing import List, Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]
+async def async_comprehension() -> List[float]:
     """Basic async comprehension."""
     result = [i async for i in async_generator() if i < 10]
     return (result)
