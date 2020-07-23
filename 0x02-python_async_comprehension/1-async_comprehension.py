b25401b542b5fc7f5e45170d9f58c14971a06f99
jfkquality <jfkquality@gmail.com>
2020-07-23 04:17:21 +0000

    Task 1, bugfix: Put back Generator return type.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,13 +2,13 @@
 """ asynch comprehension."""
 
 import asyncio
-from typing import List
+from typing import List, Generator
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]
+async def async_comprehension() -> Generator[float, None, None]
     """Basic async comprehension."""
     result = [i async for i in async_generator()]
     return (result)
