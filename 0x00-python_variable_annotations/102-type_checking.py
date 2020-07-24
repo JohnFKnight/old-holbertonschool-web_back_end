8b3081bb0660ae23e9a9a270037b5739ed02138f
jfkquality@ <jfkquality@gmail..com>
2020-07-24 03:12:06 +0000

    Task 12/102, bugfix. FINALLY figured to convert lists and tuples!

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
 """ Sample code. Correct and annotate."""
 
-from typing import Tuple, List, Union
+from typing import Tuple, List, Union, Any
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """ Sample code. Correct and annotate."""
-    john: Tuple[...] = (
-        item for item in lst
+    john: List = [
+        item for item in list(lst)
         for i in range(factor)
-    )
+    ]
+    # list(john)
     return (john)
 
 
 array = [12, 72, 91]
 print(array)
 
-zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(tuple(array))
 print(zoom_2x)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(tuple(array), 3)
 print(zoom_3x)
