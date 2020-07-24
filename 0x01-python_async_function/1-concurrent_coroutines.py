43b7461eb3ba4680e8076a0cc121977f0c02a966
jfkquality@ <jfkquality@gmail..com>
2020-07-24 03:46:56 +0000

    Task 1, bugfix: Put back default max delay mistakenly removed. Should help task 2.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """execute multiple coroutines at the same time with async."""
     list1: List[float] = []
     dly_list: List[float] = []
