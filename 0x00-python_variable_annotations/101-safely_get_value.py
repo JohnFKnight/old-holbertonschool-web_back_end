5b7b5749680cb27145a79bd2f298170c35796d49
jfkquality <jfkquality@gmail.com>
2020-07-24 00:12:23 +0000

    Task 11/101, bugfix: Linter, mypy errs. INFURIATINGLY VAGUE.

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """Make sample code annoted."""
 
-
 from typing import Any, Union, Mapping, TypeVar
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), type(None)]) -> Union[Any, TypeVar('T')]:
+T = TypeVar('T', bound=Any)
+
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union['T', None] = None) -> Union[Any, 'T']:
     """Make sample code annoted Duck typing."""
     if key in dct:
         return dct[key]
