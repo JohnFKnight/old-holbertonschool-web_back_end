378c3366350d88e84019ad9e2a42269def54de49
jfkquality@ <jfkquality@gmail..com>
2020-07-24 03:23:13 +0000

    Task 12/102, bugfix: Remove debugging print stmts.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,19 +6,15 @@ from typing import Tuple, List, Union, Any
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """ Sample code. Correct and annotate."""
-    john: List = [
+    zoomed_in: List = [
         item for item in list(lst)
         for i in range(factor)
     ]
     # list(john)
-    return (john)
-
+    return (zoomed_in)
 
 array = [12, 72, 91]
-print(array)
 
 zoom_2x = zoom_array(tuple(array))
-print(zoom_2x)
 
 zoom_3x = zoom_array(tuple(array), 3)
-print(zoom_3x)
