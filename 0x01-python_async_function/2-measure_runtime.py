8991d2714ba6526b7b4942e4a71b3f48dff3ed22
jfkquality@ <jfkquality@gmail..com>
2020-07-24 04:06:02 +0000

    Task 2, bugfix: IT'S NOT AN ASYNC FUNCTION; IT'S A REGULAR FUNCTION.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,13 +6,12 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int = 10) -> float:
+def measure_time(n: int, max_delay: int = 10) -> float:
     """execute multiple coroutines at the same time with async."""
 
     elaspsed: float
 
     s = time.perf_counter()
-    # await wait_n(n, max_delay)
     asyncio.run(wait_n(n, max_delay))
     elapsed = time.perf_counter() - s
     return (elapsed / n)
