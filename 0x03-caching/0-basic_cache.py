4ef87939193333cd8bbbd76a25a79fef960ea62d
jfkquality <jfkquality@gmail.com>
2020-07-27 20:39:14 +0000

    Task 0, bugfix: Linter errors: add doc strings to function defs.

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -10,14 +10,20 @@ class BasicCache(BaseCaching):
       - where your data are stored (in a dictionary)
     """
     def __init__(self):
+        """ Initialize
+        """
         self.cache_data = {}
 
     def put(self, key, item):
+        """ Add to cache disctionary
+        """
         if (key is None or item is None):
             pass
         self.cache_data[key] = item
 
     def get(self, key):
+        """ Get value from cache
+        """
         if (key is None or not key):
             pass
         return self.cache_data.get(key)
