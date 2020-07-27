8b8f7313847324ed35639926fb2b7e03ee3f83e6
jfkquality <jfkquality@gmail.com>
2020-07-27 21:12:48 +0000

    Task 0, bugfix: Remove parens around if stmts to get 2 red checks?

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -9,21 +9,17 @@ class BasicCache(BaseCaching):
       - constants of your caching system
       - where your data are stored (in a dictionary)
     """
-    # def __init__(self):
-    #     """ Initialize
-    #     """
-    #     self.cache_data = {}
 
     def put(self, key, item):
         """ Add to cache disctionary
         """
-        if (key is None or item is None):
+        if key is None or item is None:
             pass
         self.cache_data[key] = item
 
     def get(self, key):
         """ Get value from cache
         """
-        if (key is None or not key):
+        if key is None or not key:
             pass
         return self.cache_data.get(key)
