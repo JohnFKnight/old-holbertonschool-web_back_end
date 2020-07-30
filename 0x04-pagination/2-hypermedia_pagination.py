f5fe2c6eed373f7e164938de229379ce53bc7f87
jfkquality <jfkquality@gmail.com>
2020-07-30 06:42:54 +0000

    Task 1, bugfix: Return correct output.

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -36,15 +36,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Paginate csv file
-        """
         assert isinstance(page, int)
         assert isinstance(page_size, int)
         assert page > 0
         assert page_size > 0
 
-        idx_range: tuple
-        idx_range = index_range(page, page_size)
 
-        names = self.dataset()
-        return names[idx_range[0]:idx_range[1]]
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[List]:
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+
