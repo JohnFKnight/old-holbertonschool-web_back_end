fff7c547227a8fa15934d246a754e8df47ba0990
jfkquality <jfkquality@gmail.com>
2020-07-30 04:08:06 +0000

    Task 0, Linter errors: Fix blank lines.

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -4,10 +4,10 @@
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return 2 member tuple of start, end index
     """
-
 
     res: tuple
     res = ((page - 1) * page_size, page * page_size)
