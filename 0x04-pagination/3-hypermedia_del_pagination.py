415528b82e4c05d77e13ea657ea736511c60e974
jfkquality <jfkquality@gmail.com>
2020-08-01 01:51:27 +0000

    Task 3, Init commit 2: Forgot 3-main.py. Put main code into task code; fix.

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python3
 """
-Main file
+Deletion-resilient hypermedia pagination
 """
 
-Server = __import__('2-hypermedia_pagination').Server
+import csv
+import math
+from typing import List
 
-server = Server()
 
-print(server.get_hyper(1, 2))
-print("---")
-print(server.get_hyper(2, 2))
-print("---")
-print(server.get_hyper(100, 3))
-print("---")
-print(server.get_hyper(3000, 100))
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+                self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        pass
