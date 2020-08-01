d051a2d8d15952b5b14560ed821641b7a06ab854
jfkquality <jfkquality@gmail.com>
2020-08-01 01:41:09 +0000

    Task 3, Init commit: base code. Tasks 1-2, refactor: res: Tuple not tuple.

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -9,6 +9,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return 2 member tuple of start, end index
     """
 
-    res: tuple
+    res: Tuple
     res = ((page - 1) * page_size, page * page_size)
     return res
--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -11,7 +11,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return 2 member tuple of start, end index
     """
 
-    res: tuple
+    res: Tuple
     res = ((page - 1) * page_size, page * page_size)
     return res
 
