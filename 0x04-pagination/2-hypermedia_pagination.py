e016b017125193920dff0c6daf23a867c78e54eb
jfkquality <jfkquality@gmail.com>
2020-08-02 00:40:19 +0000

    Task 0-2, Refactor: change tuple to Tuple for proper annotation.

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -11,7 +11,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return 2 member tuple of start, end index
     """
 
-    res: tuple
+    res: Tuple
     res = ((page - 1) * page_size, page * page_size)
     return res
 
@@ -43,7 +43,7 @@ class Server:
         assert page > 0
         assert page_size > 0
 
-        idx_range: tuple
+        idx_range: Tuple
         idx_range = index_range(page, page_size)
 
         names = self.dataset()
