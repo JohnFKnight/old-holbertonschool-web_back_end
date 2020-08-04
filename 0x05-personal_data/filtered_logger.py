7cce37f2b746904f93ccc8995ece72d845ccb19d
jfkquality <jfkquality@gmail.com>
2020-08-04 00:06:31 +0000

    Task 0, bufix: Remove long comments. Add return annoation, str.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,16 +6,11 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str):
+                 message: str, separator: str) -> str:
     """
     Mask field values with redaction.
     Return message with masked field values, delimited by separator.
     """
-
-    # Convert message to dict (split on =)
-    # for field in fields: dmsg[field] = redaction
-    # Convert dmsg back to list: message dilimited by separator
-    # Return message
 
     msg1 = message.split(";")
     del msg1[len(msg1) - 1]
@@ -28,8 +23,6 @@ def filter_datum(fields: List[str], redaction: str,
     message2.append("")
 
     for k, v in dmsg.items():
-        # message2.append(k + "=" + v)
         message2[0] += k + "=" + v + ";"
 
-    # message2.replace(',', ';')
     return message2[0]
