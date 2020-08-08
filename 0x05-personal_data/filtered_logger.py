16c86a411d535d175d09ca132e552f29debca73f
jfkquality <jfkquality@gmail.com>
2020-08-08 20:10:09 +0000

    Task 1. Init commit: Unfinished (barely started).

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,8 +3,24 @@
 """
 
 import re
+import logging
 from typing import List
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
@@ -15,4 +31,5 @@ def filter_datum(fields: List[str], redaction: str,
     for field in fields:
         message = re.sub(field + "(.+?)" + separator,
                          field + "=" + redaction + separator, str(message))
+
     return message
