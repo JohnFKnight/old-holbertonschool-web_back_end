25f3931117ddc0d9bdc6f4e0bd9276173dac0070
jfkquality <jfkquality@gmail.com>
2020-08-09 03:55:14 +0000

    Task 3, bugfix: Linter errors.

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -4,19 +4,20 @@
 from flask import request   # , jsonify, abort
 # from api.v1.views import app_views
 
+
 class Auth():
+    """ Autho class
+    """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Public method require_auth
         """
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """ Public method auth...header
         """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Request current user
