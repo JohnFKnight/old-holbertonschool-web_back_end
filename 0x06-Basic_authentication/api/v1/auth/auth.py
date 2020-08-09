28fd61a24e09bb538ff614bce4fc6302f4f4469f
jfkquality <jfkquality@gmail.com>
2020-08-09 05:47:59 +0000

    Task 4. Init commit.

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request   # , jsonify, abort
 from typing import List, TypeVar
+import re
 # from api.v1.views import app_views
 
 
@@ -13,7 +14,15 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Public method require_auth
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Public method auth...header
