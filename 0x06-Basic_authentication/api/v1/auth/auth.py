f640c739d5e465aeba4c2c537c1403910ee1aec7
jfkquality <jfkquality@gmail.com>
2020-08-09 03:52:40 +0000

    Task 3. Init commit.

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 """ Module of API Authentication views
 """
-from flask import jsonify, abort
-from api.v1.views import app_views
+from flask import request   # , jsonify, abort
+# from api.v1.views import app_views
+
+class Auth():
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Public method require_auth
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Public method auth...header
+        """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Request current user
+        """
+        return None
