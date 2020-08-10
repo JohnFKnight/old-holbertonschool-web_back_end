3ff80e976ebfb4b6032a8c31093f3d811143a4ee
jfkquality <jfkquality@gmail.com>
2020-08-10 06:55:10 +0000

    Task 7. Init commit.

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 """ Module of API Authentication views
 """
-from flask import request   # , jsonify, abort
+from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """ Basic Auth class
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Base 64 auth header
+        """
+
+        if ((authorization_header is None)
+                or (not isinstance(authorization_header, str)) or
+                (re.search("^Basic ", str(authorization_header)) is None)):
+            return None
+        else:
+            # print("IN THE ELSE")
+            try:
+                return re.search("(?<=Basic ).*",
+                                 str(authorization_header)).group()
+            except Exception:
+                return re.search("(?<=Basic ).*",  str(authorization_header))
