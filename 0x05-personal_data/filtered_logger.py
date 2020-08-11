02f30e30bfb42dfb31aec4d716e5d7d97cfd8da3
jfkquality <jfkquality@gmail.com>
2020-08-11 18:29:48 +0000

    Task 1, bufix: Annotate fields parameter in init.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -32,23 +32,18 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """ Init constructor
         """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-    # def format(self, record: logging.LogRecord) -> str:
-    #     """ Filter, format record.
-    #     """
-    #     record.msg = filter_datum(self.fields, self.REDACTION,
-    #                               record.getMessage(), self.SEPARATOR)
-    #     return super().format(record)
-
     def format(self, record: logging.LogRecord) -> str:
-        """ format """
-        mess = logging.Formatter(self.FORMAT).format(record)
-        return filter_datum(self.fields, self.REDACTION, mess, self.SEPARATOR)
+        """ Filter, format record.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
 
 
 def filter_datum(fields: List[str], redaction: str,
