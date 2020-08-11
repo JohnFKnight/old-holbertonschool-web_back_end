8457b8c856bd963a076cc1c55a9a5d4a7fc744b3
jfkquality <jfkquality@gmail.com>
2020-08-11 18:06:49 +0000

    Task 1, bufix. Try Wills code.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -38,12 +38,17 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
+    # def format(self, record: logging.LogRecord) -> str:
+    #     """ Filter, format record.
+    #     """
+    #     record.msg = filter_datum(self.fields, self.REDACTION,
+    #                               record.getMessage(), self.SEPARATOR)
+    #     return super().format(record)
+
     def format(self, record: logging.LogRecord) -> str:
-        """ Filter, format record.
-        """
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
-        return super().format(record)
+        """ format """
+        mess = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, mess, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
