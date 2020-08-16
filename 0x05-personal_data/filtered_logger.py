a9597251de9a3724a02c95c72df897bcfd014b49
jfkquality <jfkquality@gmail.com>
2020-08-16 03:01:51 +0000

    Task 2. Actual init commit.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,23 +5,9 @@
 import re
 import logging
 from typing import List
-import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
@@ -46,12 +32,14 @@ class RedactingFormatter(logging.Formatter):
                                   record.getMessage(), self.SEPARATOR)
         return super().format(record)
 
-    def get_logger():
-        RedactingFormatter = __import__('filtered_logger').RedactingFormatter
 
-        message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-        log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
-        formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+def get_logger() -> logging.Logger:
+    handler = logging.StreamHandler()
+    log_record = logging.LogRecord("\"user_data",
+                                   logging.INFO, None, None, None, None, None)
+    log_record.addHandler(handler)
+    formatter = logging.Formatter(fields=(self.PII_FIELDS))
+    return formatter.format(log_record)
 
 
 def filter_datum(fields: List[str], redaction: str,
