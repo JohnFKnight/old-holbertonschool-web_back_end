a0c870e67b6ae53cd8346c7ef749a26e128141c4
jfkquality <jfkquality@gmail.com>
2020-08-19 00:33:17 +0000

    Task 0. Init commit.

---
+++ 0x08-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Create User model with SQLAlchemy
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User subclass of Base"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    # id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=True)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
