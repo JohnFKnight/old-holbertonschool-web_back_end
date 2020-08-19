fbe473d20f584fd58facb7607a6e5fd1a1c91a97
jfkquality <jfkquality@gmail.com>
2020-08-19 03:55:08 +0000

    Task 1. Init commit: Unfinished. Just to push from container.

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from user import Base
+
+
+class DB:
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+
+    def add_user(self, email: str, hashed_password: str):  # -> User()
+        self.email = email
+        self.hpwd = hashed_password
+        DB._session()
