19415807ec10e02c37a45fe4deb286cdaf387599
jfkquality <jfkquality@gmail.com>
2020-08-20 05:38:55 +0000

    Task 6. Init commit. Works.

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,7 +3,6 @@
 """
 
 import bcrypt
-
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -21,9 +20,11 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """ Register user
         """
+    def register_user(self, email: str, password: str) -> User:
+        """ check if a user exists, otherwise register """
         try:
             self._db.find_user_by(email=email)
-            raise ValueError('User ' + email + ' already exists')
+            raise ValueError  # ('User ' + email + ' already exists')
         except Exception:
             hpwd = _hash_password(password)
             return self._db.add_user(email, hpwd)
