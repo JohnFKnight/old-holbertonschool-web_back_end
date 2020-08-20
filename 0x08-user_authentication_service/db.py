a3e624023d02bfb331003f472c2a8a13ec5d6921
jfkquality <jfkquality@gmail.com>
2020-08-20 19:11:02 +0000

    Task 3. Bugfix: Linter err.

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" DB class to Add, find, update users 
+""" DB class to Add, find, update users
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
