87dd31c9c19ec92e48d8013f401056359c050dbf
jfkquality <jfkquality@gmail.com>
2020-08-23 15:46:58 +0000

    Task 7. Bugfix: Linter errors.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -45,7 +45,6 @@ def register_route() -> str:
 
     AUTH = Auth()
 
-
     body = request.form
 
     try:
@@ -53,6 +52,7 @@ def register_route() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
