dc0312560ecdc4fc938505bbbac17fec6dc15561
jfkquality <jfkquality@gmail.com>
2020-08-23 16:48:53 +0000

    Task 7. Bugfix? Move auth import and instance assignment to top of file.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,6 +3,9 @@
 """
 
 from flask import Flask, jsonify, request, make_response
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -19,19 +22,16 @@ def hello_world():
 def users() -> str:
     """ Authenticate user
     """
-    from auth import Auth
-
-    AUTH = Auth()
 
     try:
         user = AUTH.register_user(
             request.form['email'], request.form['password'])
-        return jsonify({"email": user.email,
-                        "message": "user created"})
+        return {"email": user.email,
+                "message": "user created"}
     except ValueError as err:
         # return make_response( jsonify(
         # {"message": "email already registered"}), 400)
-        return jsonify({"message": "email already registered"}, 400)
+        return {"message": "email already registered"}, 400
 
 
 if __name__ == "__main__":
