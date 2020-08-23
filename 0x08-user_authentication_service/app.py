81f30b5b80ce072758bf0993019edfe414b16e10
jfkquality <jfkquality@gmail.com>
2020-08-23 16:00:29 +0000

    Task 7. Bugfix: Linter errs? Removed commented function declaration.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -15,43 +15,23 @@ def hello_world():
     return {"message": "Bienvenue"}
 
 
-# @app.route('/users/<email>/<password>',
-#            methods=['GET', 'POST'], strict_slashes=False)
-# def users(*, email, password):
-# @app.route("/users", methods=['POST'], strict_slashes=False)
-# def users() -> str:
-#     """ Authenticate user
-#     """
-    # from auth import Auth
-
-    # AUTH = Auth()
-
-#     try:
-#         user = AUTH.register_user(
-#             request.form['email'], request.form['password'])
-#         return {"email": user.email,
-#                 "message": "user created"}
-#     except ValueError as err:
-#         # return make_response( jsonify(
-#         # {"message": "email already registered"}), 400)
-#         return {"message": "email already registered"}, 400
-
 @app.route("/users", methods=['POST'], strict_slashes=False)
-def register_route() -> str:
-    """ get the register route
+def users() -> str:
+    """ Authenticate user
     """
-
     from auth import Auth
 
     AUTH = Auth()
 
-    body = request.form
-
     try:
-        user = AUTH.register_user(body['email'], body['password'])
-        return jsonify({"email": user.email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+        user = AUTH.register_user(
+            request.form['email'], request.form['password'])
+        return {"email": user.email,
+                "message": "user created"}
+    except ValueError as err:
+        # return make_response( jsonify(
+        # {"message": "email already registered"}), 400)
+        return {"message": "email already registered"}, 400
 
 
 if __name__ == "__main__":
