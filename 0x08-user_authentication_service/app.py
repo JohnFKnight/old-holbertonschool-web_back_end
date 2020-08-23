4eff902fa2f328f44af4debab06648ad0d998b16
jfkquality <jfkquality@gmail.com>
2020-08-23 15:45:17 +0000

    Task 7. Bugfix: Try Wills code. Same output as mine.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -18,24 +18,41 @@ def hello_world():
 # @app.route('/users/<email>/<password>',
 #            methods=['GET', 'POST'], strict_slashes=False)
 # def users(*, email, password):
+# @app.route("/users", methods=['POST'], strict_slashes=False)
+# def users() -> str:
+#     """ Authenticate user
+#     """
+    # from auth import Auth
+
+    # AUTH = Auth()
+
+#     try:
+#         user = AUTH.register_user(
+#             request.form['email'], request.form['password'])
+#         return {"email": user.email,
+#                 "message": "user created"}
+#     except ValueError as err:
+#         # return make_response( jsonify(
+#         # {"message": "email already registered"}), 400)
+#         return {"message": "email already registered"}, 400
+
 @app.route("/users", methods=['POST'], strict_slashes=False)
-def users() -> str:
-    """ Authenticate user
+def register_route() -> str:
+    """ get the register route
     """
+
     from auth import Auth
 
     AUTH = Auth()
 
-    try:
-        user = AUTH.register_user(
-            request.form['email'], request.form['password'])
-        return {"email": user.email,
-                "message": "user created"}
-    except ValueError as err:
-        # return make_response( jsonify(
-        # {"message": "email already registered"}), 400)
-        return {"message": "email already registered"}, 400
 
+    body = request.form
+
+    try:
+        user = AUTH.register_user(body['email'], body['password'])
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
