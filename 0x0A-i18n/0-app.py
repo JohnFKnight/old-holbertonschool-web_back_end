f917201b0e3c290cc47b857147065d2870798bb9
jfkquality <jfkquality@gmail.com>
2020-08-28 21:42:20 -0400

    Task 0. Bugfix: Put back all my original code, Jinga and all

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Base route."""
-    return render_template('0-index.html')
+    return render_template(('0-index.html'),
+                           title="Welcome to Holberton",
+                           h1="Hello world"
 
 
-# if __name__ == '__main__':
-#     app.run(host='127.0.0.1', port='5000')
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000')
