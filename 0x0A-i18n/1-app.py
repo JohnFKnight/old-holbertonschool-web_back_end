52964b9979521659e899325d77e0262d5d4f0bf8
jfkquality <jfkquality@gmail.com>
2020-08-28 23:19:58 -0400

    Task 1. Init commit. Working?

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Flask app
+"""
+
+from flask import Flask, render_template, g, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config.from_object('Config')
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """Base route."""
+    return render_template('0-index.html')
+
+
+class Config:
+    """ Config class for app/babel config."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000')
