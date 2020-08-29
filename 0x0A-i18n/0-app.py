e3c8142aef3574ca74dee547d6ab29c202e62173
jfkquality <jfkquality@gmail.com>
2020-08-28 20:28:24 -0400

    Task 0. Actual init commit. Working.

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Flask app
+"""
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """Base route."""
+    return render_template(('0-index.html'),
+        title="Welcome to Holberton",
+        h1 ="Hello world"
+    )
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000')
