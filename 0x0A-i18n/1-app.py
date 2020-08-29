3e50aba5d6ba8a7b2f73aa71975c930f0638dcdf
jfkquality <jfkquality@gmail.com>
2020-08-29 00:06:01 -0400

    Task 1. Bugfix> Correctly indent Garys code.

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -15,18 +15,18 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
     # set the above class object as the configuration for the app
     app.config.from_object('1-app.Config')
 
 
-    @app.route('/', methods=['GET'], strict_slashes=False)
-    def index() -> str:
-        """ GET /
-        Return:
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return:
         - 1-index.html
-        """
-        return render_template('1-index.html')
+    """
+    return render_template('1-index.html')
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port='5000')
