f594739f2b6f573b4eb6e2784cb1ebc6b550aecd
jfkquality <jfkquality@gmail.com>
2020-08-28 23:52:43 -0400

    Task 1. Bugfix:  Put app.congif back below class. Change double quotes to single.

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -3,9 +3,7 @@
 """
 
 from flask import Flask, render_template, g, request
-app.config.from_object('1-app.Config')
 from flask_babel import Babel
-# from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -19,10 +17,12 @@ def index():
 
 class Config(object):
     """ Config class for app/babel config."""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
+app.config.from_object('1-app.Config')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000')
