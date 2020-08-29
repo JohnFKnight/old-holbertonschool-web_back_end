0b4643192f367885d8193a0b312a1b809f09057e
jfkquality <jfkquality@gmail.com>
2020-08-29 02:06:14 -0400

    Task 1. Bug guess: Use Garys app.py code.

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-""" Flask app
-"""
-
-from flask import Flask, render_template, g, request
+""" Route module for the API """
+from flask import Flask, request, render_template
 from flask_babel import Babel
 from os import getenv
 
@@ -11,18 +9,23 @@ babel = Babel(app)
 
 
 class Config(object):
-    """ Config class for app/babel config."""
+    """ Babel configuration """
     LANGUAGES = ['en', 'fr']
+    # these are the inherent defaults just btw
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# set the above class object as the configuration for the app
 app.config.from_object('1-app.Config')
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
-    """Base route."""
+    """ GET /
+    Return:
+      - 1-index.html
+    """
     return render_template('1-index.html')
 
 
@@ -30,7 +33,3 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
-
-# if __name__ == '__main__':
-#     app.run(host='127.0.0.1', port='5000')
