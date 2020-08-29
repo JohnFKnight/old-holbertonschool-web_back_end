dde54fd63a6b72247014b6540070f23ea237078f
jfkquality <jfkquality@gmail.com>
2020-08-29 00:02:50 -0400

    Task 1. Bugfux guess: Try Garys code.

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
-""" Flask app
-"""
-
-from flask import Flask, render_template, g, request
+""" Route module for the API """
+from flask import Flask, request, render_template
 from flask_babel import Babel
+from os import getenv
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config(object):
-    """ Config class for app/babel config."""
+    """ Babel configuration """
     LANGUAGES = ['en', 'fr']
+    # these are the inherent defaults just btw
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object('1-app.Config')
+    # set the above class object as the configuration for the app
+    app.config.from_object('1-app.Config')
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """Base route."""
-    return render_template('1-index.html')
+    @app.route('/', methods=['GET'], strict_slashes=False)
+    def index() -> str:
+        """ GET /
+        Return:
+        - 1-index.html
+        """
+        return render_template('1-index.html')
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='127.0.0.1', port='5000')
