68e2dac744b44c0f63e9e03b657e9e1d23071b18
jfkquality <jfkquality@gmail.com>
2020-09-01 14:49:12 -0400

    Task 0. Bugfix: Try even better syntax to update redis db.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,11 +7,11 @@ import uuid
 class Cache():
     def __init__(self):
         """Class Constructor."""
-        self._redis = redis()
+        self._redis = redis.Redis()
         self._redis.flushdb
 
     def storage(data: str) -> str:
         """Store data in redis db."""
         key = str(uid.uuid4())
-        self._redis[key] = data
+        self._redis.set({key: data})
         return key
