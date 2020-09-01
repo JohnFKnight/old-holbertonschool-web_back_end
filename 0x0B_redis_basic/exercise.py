b60365ef4a595898ecccc331c215adf50c4a9eae
jfkquality <jfkquality@gmail.com>
2020-09-01 15:59:26 -0400

    Task 0. Linter errors. Add missing doc strings on module, class.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+""" Redis exercise """
 
 import redis
 import uuid
 
 
 class Cache():
+    """ Cache class """
     def __init__(self):
         """Class Constructor."""
         self_redis = redis.Redis()
@@ -13,6 +15,6 @@ class Cache():
     def storage(data: str) -> str:
         """Store data in redis db."""
         k = str(uid.uuid4())
-        self._redis.sadd({k: data})
+        self._redis.set({k: data})
         # self._redis.bgsave()
         return k
