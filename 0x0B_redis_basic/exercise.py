3fee1483edf73ae0fa19c85129f2d4bf378ce25f
jfkquality <jfkquality@gmail.com>
2020-09-01 22:03:34 -0400

    Task 0. Bugfix: Fix various errors, typos.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,18 +3,25 @@
 
 import redis
 import uuid
+from typing import Union
 
 
 class Cache():
     """ Cache class """
     def __init__(self):
         """Class Constructor."""
-        self_redis = redis.Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb
 
-    def store(self, data: Union(str, bytes, int, float)) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis db."""
-        k = (uid.uuid4())
+        k = str(uuid.uuid4())
         self._redis.set(k, data)
-        self._redis.bgsave()
+        # self._redis.bgsave()
+        # print(self._redis.get(k))
         return k
+
+
+if __name__ == "__main__":
+    cache = Cache()
+    cache.store("hell0")
