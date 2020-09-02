5b92ffc94da09b7ee434f884734c89e6ecfe4780
jfkquality <jfkquality@gmail.com>
2020-09-01 22:16:46 -0400

    Task 0. Bugfix: Redo redis.bgsave.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -17,11 +17,11 @@ class Cache():
         """Store data in redis db."""
         k = str(uuid.uuid4())
         self._redis.set(k, data)
-        # self._redis.bgsave()
+        self._redis.bgsave()
         # print(self._redis.get(k))
         return k
 
 
-# if __name__ == "__main__":
-#     cache = Cache()
-#     cache.store("hell0")
+if __name__ == "__main__":
+    cache = Cache()
+    cache.store("hell0")
