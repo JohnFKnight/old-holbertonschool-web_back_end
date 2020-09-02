56c1cf9fff0aa84703970f0921052b7dfb955723
jfkquality <jfkquality@gmail.com>
2020-09-01 21:24:05 -0400

    Task 0. Testing: Comment out method.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,9 +12,9 @@ class Cache():
         self_redis = redis.Redis()
         self._redis.flushdb
 
-    def store(self, data: list) -> str:
-        """Store data in redis db."""
-        k = str(uid.uuid4())
-        self._redis.hmset({k: data})
-        # self._redis.bgsave()
-        return k
+    # def store(self, data: list) -> str:
+    #     """Store data in redis db."""
+    #     k = str(uid.uuid4())
+    #     self._redis.hmset({k: data})
+    #     # self._redis.bgsave()
+    #     return k
