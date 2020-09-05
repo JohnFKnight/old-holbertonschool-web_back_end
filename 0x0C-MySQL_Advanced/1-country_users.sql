ecc2ad0d7d95c27a0dab6d0bf4f5a7987c4946c5
jfkquality <jfkquality@gmail.com>
2020-09-05 19:24:33 +0000

    Task 1. Init commit.

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,7 @@
--- Create a table, users
--- Add country list
-CREATE TABLE IF NOT EXISTS users (
-       id INT NOT NULL AUTO_INCREMENT,
-       email VARCHAR(255) NOT NULL UNIQUE,
-       name VARCHAR(255),
-       country ENUM('US', 'CO', 'TN') NOT NULL,
-       PRIMARY KEY (id)
-       )
+-- rank the country of origin of bands,
+-- ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
+SELECT origin, count(fans) AS "nb_fans"
+FROM metal_bands
+GROUP BY origin
+ORDER BY count(fans) DESC
