43b7ae7e665d79c82471bf5d33fa08cc723276cb
jfkquality <jfkquality@gmail.com>
2020-09-05 04:09:40 +0000

    Task 0. Bugfix: Add skipped ~reqired attributes to table rows.

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Create a table, users
 -- id, email, name
 CREATE TABLE IF NOT EXISTS users (
-       id INT,
+       id INT NOT NULL AUTO_INCREMENT ,
        email VARCHAR(255) NOT NULL UNIQUE,
        name VARCHAR(255),
        CNSTRAINT users_pk PRIMARY KEY (id)
