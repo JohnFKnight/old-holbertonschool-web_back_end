d47659eb87008d8c97e13dde191ce33f64479bee
jfkquality <jfkquality@gmail.com>
2020-09-05 20:05:53 +0000

    Task 2. Bugfix: Add HAVING COUNT > 1 to get only non-unique rows.

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,8 @@
 -- rank the country of origin of bands,
 -- ordered by the number of (non-unique) fans
 -- Column names must be: origin and nb_fans
-SELECT origin, count(fans) AS "nb_fans"
+SELECT origin, COUNT(fans) AS "nb_fans"
 FROM metal_bands
 GROUP BY origin
-ORDER BY count(fans) DESC
+HAVING COUNT(fans) > 1
+ORDER BY COUNT(fans) DESC;
