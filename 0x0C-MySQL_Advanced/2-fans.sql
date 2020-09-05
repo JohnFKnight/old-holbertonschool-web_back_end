5930e890ecbaf6de9ae3befbb4509e87f0d58262
jfkquality <jfkquality@gmail.com>
2020-09-05 19:50:22 +0000

    Task 2. Debug: Delete task 1 sql!

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -5,13 +5,3 @@ SELECT origin, count(fans) AS "nb_fans"
 FROM metal_bands
 GROUP BY origin
 ORDER BY count(fans) DESC
-
--- Create a table, users
--- Add country list
-CREATE TABLE IF NOT EXISTS users (
-       id INT NOT NULL AUTO_INCREMENT,
-       email VARCHAR(255) NOT NULL UNIQUE,
-       name VARCHAR(255),
-       country ENUM('US', 'CO', 'TN'),
-       PRIMARY KEY (id)
-       )
