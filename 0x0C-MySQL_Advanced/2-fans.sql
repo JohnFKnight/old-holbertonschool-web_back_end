c02e128eeb1e8042291a26b4b3b7153d0cb81aeb
jfkquality <jfkquality@gmail.com>
2020-09-05 20:32:51 +0000

    Task 2. Bugfix: USE SUM NOT COUNT!

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,7 @@
 -- rank the country of origin of bands,
 -- ordered by the number of (non-unique) fans
 -- Column names must be: origin and nb_fans
-SELECT origin, COUNT(fans) AS "nb_fans"
+SELECT origin, SUM(fans) AS "nb_fans"
 FROM metal_bands
 GROUP BY origin
-HAVING COUNT(fans) > 1
-ORDER BY COUNT(fans) DESC;
+ORDER BY nb_fans DESC;
