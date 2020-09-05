1349be7ff024e94d0d58d7b43eefb020fce64b2d
jfkquality <jfkquality@gmail.com>
2020-09-05 19:32:00 +0000

    Task 2. Init commit. Restore task 1 (replaced it with task 2).

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,11 @@
+-- rank the country of origin of bands,
+-- ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
+SELECT origin, count(fans) AS "nb_fans"
+FROM metal_bands
+GROUP BY origin
+ORDER BY count(fans) DESC
+
 -- Create a table, users
 -- Add country list
 CREATE TABLE IF NOT EXISTS users (
