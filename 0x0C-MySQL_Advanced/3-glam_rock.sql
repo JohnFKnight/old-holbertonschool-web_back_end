cd342522b1790029ae6d90fcef6469b1fff49da2
jfkquality <jfkquality@gmail.com>
2020-09-06 15:27:45 +0000

    Task 3. Refactor: Try CURRENT_DATE vs CURDATE().

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -3,7 +3,7 @@
 -- Column names must be: band_name and lifespan (in years)
 -- You should use attributes formed and split for computing the lifespan
 SELECT band_name,
-       IFNULL(split, YEAR(CURDATE())) - formed AS "lifespan"
+       IFNULL(split, YEAR(CURRENT_DATE())) - formed AS "lifespan"
 FROM metal_bands
 WHERE   style LIKE '%Glam%'
 GROUP BY band_name, lifespan
