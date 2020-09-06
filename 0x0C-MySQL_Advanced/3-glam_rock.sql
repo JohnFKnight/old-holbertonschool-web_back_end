e23a26796f406ad9c8d31f456ec736d555092f25
jfkquality <jfkquality@gmail.com>
2020-09-06 15:22:18 +0000

    Task 3. Container push before it expires.

---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,9 @@
+-- List bands by glam style
+-- Ranked by their longevity
+-- Column names must be: band_name and lifespan (in years)
+-- You should use attributes formed and split for computing the lifespan
+SELECT band_name,
+       IFNULL(split, YEAR(CURDATE())) - formed AS "lifespan"
+FROM metal_bands
+WHERE   style LIKE '%Glam%'
+GROUP BY band_name, lifespan
