6156c36e951792e1167918b88c310238996cc6fb
jfkquality <jfkquality@gmail.com>
2020-09-08 19:10:03 -0700

    Task 10. Bugfix: Remove  true, false parameters again.

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -9,4 +9,4 @@ def update_topics(mongo_collection, name, topics):
     if not mongo_collection:
         return []
     return mongo_collection.update({"name": name},
-                                   {"$set": {"topics": topics}}, "false", "true")
+                                   {"$set": {"topics": topics}})
