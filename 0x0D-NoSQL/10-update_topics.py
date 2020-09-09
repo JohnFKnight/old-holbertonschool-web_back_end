343cc6bebadf0db47b86080abf509db60861fc4f
jfkquality <jfkquality@gmail.com>
2020-09-08 18:55:27 -0700

    Task 10. Bugfix: Put quotes around keys.

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -8,5 +8,5 @@ def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name """
     if not mongo_collection:
         return []
-    return mongo_collection.update({name: name},
-                                   {$set: {topics: topics}}, false, true)
+    return mongo_collection.update({"name": name},
+                                   {$set: {"topics": topics}}, false, true)
