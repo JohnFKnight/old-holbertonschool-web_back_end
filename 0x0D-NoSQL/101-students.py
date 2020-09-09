b6b10e16ecbfc36c71d461ce7bb9a9c8cdd9980e
jfkquality <jfkquality@gmail.com>
2020-09-08 21:59:08 -0700

    Task 14/101. Bugfix: Add unwind to unwind the topics list.

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -6,17 +6,16 @@ def top_students(mongo_collection):
     """ returns all students sorted by average score."""
     # return list(mongo.collection.find())
 
-    return mongo_collection.aggregate([
-        {"$group": {_id: "$name", score: {"$avg", 1}}},
-        {"$sort": {_id, -1}}])
+    # return mongo_collection.aggregate([
+    #     {"$group": {_id: "$name", score: {"$avg", 1}}},
+    #     {"$sort": {_id, -1}}])
 
-    # pipeline = [
-    # {$unwind: "$topics"},
-    # {$group: {
-    #     _id: "$topics[$score]",
-    #     score: {$avg, 1}
-    # }},
-    # {$group: {
-    #     avg_score: { $avg: "$score" }
-    # }}
-    # ])
+    pipeline = [
+        {"$unwind": "$topics"},
+        {"$group": {
+            _id: "$topics[$score]",
+            score: {"$avg", 1}
+        }},
+        {"$sort": {_id, -1}}
+    ]
+    return mongo_collection.aggregate(pipeline)
