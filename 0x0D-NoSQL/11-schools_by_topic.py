b8fb4aa76fecd348f4182fab8f45e3b432572113
jfkquality <jfkquality@gmail.com>
2020-09-08 19:16:48 -0700

    Task 11. Init commit. Untested.

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" returns the list of school having a specific topic """
+
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ returns the list of school having a specific topic: """
+    if not mongo_collection:
+        return []
+    return mongo_collection.find({"topics": topic}
