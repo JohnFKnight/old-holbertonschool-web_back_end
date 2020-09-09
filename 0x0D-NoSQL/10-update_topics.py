04bd52991bd5bea6e8835657faf3f97de4e32df5
jfkquality <jfkquality@gmail.com>
2020-09-08 19:33:48 -0700

    Task 10. Bugfix: Use updatte_many.

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name """
     if not mongo_collection:
         return []
-    mongo_collection.update({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
