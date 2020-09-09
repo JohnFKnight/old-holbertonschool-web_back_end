7ed6b9021838e25855f3262ee2bd93db51377e6d
jfkquality <jfkquality@gmail.com>
2020-09-08 18:14:34 -0700

    Task 8. Bugfix: Ad doc strings.

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+""" List all documents in a MongoDB collection """
 
 from pymongo import MongoClient
 
 def list_all(mongo_collection):
+    """ Return collections.find() """
     if not mongo_collection:
         return []
     client = MongoClient('mongodb://127.0.0.1:27017')
