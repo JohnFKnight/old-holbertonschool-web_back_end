497b3cb55a053640ddbcc02ccc6a9432d3a40059
jfkquality <jfkquality@gmail.com>
2020-09-08 18:42:02 -0700

    Task 10. Init commit. Untested.

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""  changes all topics of a school document based on the name """
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document based on the name """
+    if not mongo_collection:
+        return []
+    return mongo_collection.update( { name : name }, { $set: { topics : topics }}, false, true)    
