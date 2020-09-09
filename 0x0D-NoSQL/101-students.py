f8301eeddf32c1d12b0cf7dec042a30adb67abd7
jfkquality <jfkquality@gmail.com>
2020-09-08 21:49:23 -0700

    Task 14/101. Bugfix: Put back the word return.

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -6,7 +6,7 @@ def top_students(mongo_collection):
     """ returns all students sorted by average score."""
     # return list(mongo.collection.find())
 
-    mongo_collection.aggregate([
+    return mongo_collection.aggregate([
         {"$group": {_id: "$name", score: {"$avg", 1}}},
         {"$sort": {_id, -1}}])
 
