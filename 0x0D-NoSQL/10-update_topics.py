a90ea0ae92d1e952493f85edfbff52895f94ea63
jfkquality <jfkquality@gmail.com>
2020-09-08 19:28:35 -0700

    Task 10. Bugfix: Remove unecessary import thats been there all along.

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """  changes all topics of a school document based on the name """
 
-from pymongo import MongoClient
-
 
 def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name """
