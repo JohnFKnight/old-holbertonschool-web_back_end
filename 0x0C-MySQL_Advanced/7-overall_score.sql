e7ac01418c7752d2a2612d5b981d96f24a03a073
jfkquality <jfkquality@gmail.com>
2020-09-11 04:34:14 +0000

    Task 7. Bugfix: Try a different way.

--- 0x0C-MySQL_Advanced/7-overall_score.sql
+++ 0x0C-MySQL_Advanced/7-overall_score.sql
@@ -1,14 +1,15 @@
 -- creates a stored procedure ComputeOverallScoreForUser
 -- that stores avg score of a student.
-
+   
 DELIMITER $$
 
 CREATE PROCEDURE ComputeOverallScoreForUser(IN user_id INT)
 BEGIN
-	UPDATE users, corrections
-	SET users.overall_score = 
-	(SUM(corrections.score) / COUNT(corrections.id))
-	WHERE corrections.user_id = user_id;
+	UPDATE users
+	SET users.overall_score = (SELECT
+	SUM(score), COUNT(id), SUM(score)/COUNT(id)
+	FROM collections
+	WHERE user_id = user_id);
 END$$
 
 DELIMITER ;
