8db1db09198bc8d5ef8eabab98a52cd37fb2beb6
jfkquality <jfkquality@gmail.com>
2020-09-12 04:34:16 +0000

    Task 5. Bugfix: Use spread opearator on array1 too.

--- 0x0E-ES6_basic/11-createEmployeesObject.js
+++ 0x0E-ES6_basic/11-createEmployeesObject.js
@@ -1,17 +1,12 @@
-function createEmployeesObject(departmentName, employees) {
-    const iterable = {
-	[Symbol.iterator]() {
-	    // let step = 0;
-	    const iterator = {
-		new () {
-		    return {
-			value: departmentName.concat(...employees).toString(), done: true
-		    };
-		}
-	    };
-	    return iterator;
-	}
-    };
+export default function createEmployeesObject(departmentName, employees) {
+  console.log(departmentName, employees);
+  const iterable = {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          return { value: departmentName.concat(...employees), done: true };
+        },
+      };
+    },
+  };
 }
-
-console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
