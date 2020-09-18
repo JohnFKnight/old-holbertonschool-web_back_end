fc5ac908ed0d294f78e1349b3873b6d82a1f17e6
jfkquality <jfkquality@gmail.com>
2020-09-18 04:02:38 +0000

    Task 11: WAY overcomplicated it!

--- 0x0E-ES6_basic/11-createEmployeesObject.mjs
+++ 0x0E-ES6_basic/11-createEmployeesObject.mjs
@@ -1,3 +1,4 @@
 export default function createEmployeesObject(departmentName, employees) {
     return { [departmentName]: [...employees] };
 }
+console.log(createEmployeesObject('Software', ['Bob', 'Sylvie']));
