19fbdd364f4ac96b6c26cf13c3d6bd0c132b3f5e
jfkquality <jfkquality@gmail.com>
2020-09-18 08:02:56 +0000

    Task 12. Bugfix: NOW Get correct output. Just set AllEmployees to values.

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,8 +1,6 @@
 export default function createReportObject(employeesList) {
   const report = {
-    allEmployees: {
-      [Object.keys(employeesList)]: [Object.values(employeesList)],
-    },
+    allEmployees: employeesList,
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
