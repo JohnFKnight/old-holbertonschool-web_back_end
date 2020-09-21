166d97ceb14c34bfd0b1acf03e5ac1671b0d46d4
jfkquality <jfkquality@gmail.com>
2020-09-21 03:48:33 +0000

    Task 1. Bugfix: Move return stmt down one line.

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -10,6 +10,21 @@ export default function getFullResponseFromAPI(success) {
     } else {
       reject(Error('The fake API is not working currently'));
     }
-    return promise;
   });
+  return promise;
 }
+// export default function getFullResponseFromAPI(success) {
+//   const promise = new Promise((resolve, reject) => {
+//     if (success) {
+//       resolve(
+//         {
+//           status: 200,
+//           body: 'Success',
+//         },
+//       );
+//     } else {
+//       reject(Error('The fake API is not working currently'));
+//     }
+//   });
+//   return promise;
+// }
--- 0x0F-ES6_promise/1-promise.mjs
+++ 0x0F-ES6_promise/1-promise.mjs
@@ -2,16 +2,33 @@ export default function getFullResponseFromAPI(success) {
   const promise = new Promise((resolve, reject) => {
     if (success) {
       resolve(
-      {
-        status: 200,
-        body: 'Success',
-      })
+        {
+          status: 200,
+          body: 'Success',
+        },
+      );
     } else {
       reject(Error('The fake API is not working currently'));
     }
-  })
-  return promise;;
+  });
+  return promise;
 }
+
+// export default function getFullResponseFromAPI(success) {
+//   const promise = new Promise((resolve, reject) => {
+//     if (success) {
+//       resolve(
+//         {
+//           status: 200,
+//           body: 'Success',
+//         },
+//       );
+//     } else {
+//       reject(Error('The fake API is not working currently'));
+//     }
+//   });
+//   return promise;
+// }
 
 console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
