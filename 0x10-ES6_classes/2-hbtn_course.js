b4c919de212376e6b4a2c90664e07d0da3fa51b8
jfkquality <jfkquality@gmail.com>
2020-09-22 03:58:03 +0000

    Task 2. Init commit. Untested.

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,25 @@
+export default Class HolbertonCourse {
+  constructor(name, length, students) {
+  String this._name = name;
+  int this._length = length;
+  Array this._students = students;
+  }
+  get name() {
+    return this._name;
+  }
+  get length() {
+    return this._length;
+  }
+  get students() {
+    return this._students
+  }
+  set name(x) {
+    this._name = x;
+  }
+  set length(x) {
+    this._length = x;
+  }
+  set students(x) {
+    this._students = x;
+  }
+}
