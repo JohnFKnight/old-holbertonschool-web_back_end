f5ca8ce50568e31963c61de4036ed6a7cb0657d7
jfkquality <jfkquality@gmail.com>
2020-09-22 04:38:02 +0000

    Task 2. Bugfix: Linter errors.

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,24 +1,36 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-  String this._name = name;
-  int this._length = length;
-  Array this._students = students;
+    if (Object.prototype.toString.call(name) === '[object String]') {
+      this._name = name;
+    }
+    if (Object.prototype.toString.call(length) === '[object Number]') {
+      this._length = length;
+    }
+    if (Object.prototype.toString.call(students) === '[object Array]') {
+      this._students = students;
+    }
   }
+
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
-    return this._students
+    return this._students;
   }
+
   set name(x) {
     this._name = x;
   }
+
   set length(x) {
     this._length = x;
   }
+
   set students(x) {
     this._students = x;
   }
