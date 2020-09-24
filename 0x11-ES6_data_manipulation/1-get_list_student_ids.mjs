5014d3738d3137a439c012e745eb859467e8bc7e
jfkquality <jfkquality@gmail.com>
2020-09-24 06:58:11 +0000

    Task 1. Try different map syntax.

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.mjs
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.mjs
@@ -1,6 +1,7 @@
 export default function getListStudentIds(arr) {
-  return arr.map(() => {return arr.id;});
+  return arr.map((a) => {a.id});
 }
 import getListStudents from './0-get_list_students.mjs';
 console.log (getListStudentIds("hello"));
 // console.log (getListStudentIds(getListStudentIds()));
+// console.log ((getListStudents()));
