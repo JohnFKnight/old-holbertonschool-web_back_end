f710f7cb4a4a3c44cd4605ba101c727a0939edb8
jfkquality <jfkquality@gmail.com>
2020-09-30 02:11:01 +0000

    615 Task 1. Bugfix: Linter errors.

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,15 +1,13 @@
-process.stdin.setEncoding('utf8');
+// process.stdin.setEncoding('utf8');
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-// console.log('Welcome to Holberton School, what is your name?\n');
-
 
 process.stdin.on('readable', () => {
-    var chunk = process.stdin.read();
-    process.stdout.write('Your name is: ' + chunk);
+  const chunk = process.stdin.read();
+  process.stdout.write('Your name is: ', chunk);
 });
 
 process.on('exit', () => {
-    process.stdout.clearLine();
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.clearLine();
+  process.stdout.write('This important software is now closing\n');
 });
