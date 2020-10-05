a5e0bdf6d5454d5e1aa2c6a094aadfd7f5ccea69
jfkquality <jfkquality@gmail.com>
2020-10-05 03:58:33 +0000

    Task 0. Bugfix. More sh*t. Cnaged describe despcriptiong similar to Wills.

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,25 +2,25 @@ const assert = require ("assert");
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
-  it('Add 2 integers', function () {
+  it('should work without rounding', function () {
     assert.strictEqual(calculateNumber(3, 4), 7);
   });
-  it('Add 2 floats', function () {
+  it('should work with rounding', function () {
     assert.strictEqual(calculateNumber(3.4, 4.2), 7);
   });
-  it('Add 2 floats .0', function () {
+  it('should work without rounding', function () {
     assert.strictEqual(calculateNumber(3.0, 4.0), 7);
   });
-  it('Add neg and pos ints; pos result', function () {
+  it('should work without rounding', function () {
     assert.strictEqual(calculateNumber(-3, 4), 1);
   });
-  it('Add neg and pos ints; neg result', function () {
+  it('should work without rounding', function () {
     assert.strictEqual(calculateNumber(3, - 4), -1);
   });
-  it('Add neg and pos flosts; pos result', function () {
+  it('should work with rounding', function () {
     assert.strictEqual(calculateNumber(-3.4, 4.5), 2);
   });
-  it('Add neg and pos floats; neg result', function () {
+  it('should work with rounding', function () {
       assert.strictEqual(calculateNumber(-3.5, 2), -1);
   });
   it('should throw error if NaN passed', function () {
