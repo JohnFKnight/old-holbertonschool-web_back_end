17bb9e6675d2a0229f6827b516066ffb8bd0eaca
jfkquality <jfkquality@gmail.com>
2020-10-05 01:27:55 +0000

    Task 0, Init commit. Working.

---
+++ 0x13-unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+};
+module.exports = calculateNumber;
+
+// console.log(calculateNumber(2, 3))
+// console.log(calculateNumber(1.2, 3.3));
+// console.log(calculateNumber(2.5, 3.4));
+// console.log(calculateNumber(10.2, 11.3));
---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,31 @@
+const assert = require ("assert");
+const calcNum = require('./0-calcul.js');
+describe('Round', function () {
+  it('Add 2 integers', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+  it('Add 2 floats', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+  it('Add 2 integers', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+  it('Add 2 integers', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+  it('Add 2 integers', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+  it('Add 2 integers', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+  it('Add 2 integers', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+  it('Add 2 integers', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+  it('Add 2 integers', function () {
+    assert.equal(calcNum(3, 4), 7);
+  });
+});
