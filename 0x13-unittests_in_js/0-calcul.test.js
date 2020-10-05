098b726c3e62f42926e5d434a553e0672f721520
jfkquality <jfkquality@gmail.com>
2020-10-05 23:16:27 +0000

    Task 0. Bugfix: Change strictError to error to experiment.

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,25 +3,25 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
   it('should work without rounding', function () {
-    assert.strictEqual(calculateNumber(3, 4), 7);
+    assert.equal(calculateNumber(3, 4), 7);
   });
   it('should work with rounding', function () {
-    assert.strictEqual(calculateNumber(3.4, 4.2), 7);
+    assert.equal(calculateNumber(3.4, 4.2), 7);
   });
   it('should work without rounding', function () {
-    assert.strictEqual(calculateNumber(3.0, 4.0), 7);
+    assert.equal(calculateNumber(3.0, 4.0), 7);
   });
   it('should work without rounding', function () {
-    assert.strictEqual(calculateNumber(-3, 4), 1);
+    assert.equal(calculateNumber(-3, 4), 1);
   });
   it('should work without rounding', function () {
-    assert.strictEqual(calculateNumber(3, - 4), -1);
+    assert.equal(calculateNumber(3, - 4), -1);
   });
   it('should work with rounding', function () {
-    assert.strictEqual(calculateNumber(-3.4, 4.5), 2);
+    assert.equal(calculateNumber(-3.4, 4.5), 2);
   });
   it('should work with rounding', function () {
-      assert.strictEqual(calculateNumber(-3.5, 2), -1);
+      assert.equal(calculateNumber(-3.5, 2), -1);
   });
   it('should throw error if NaN passed', function () {
       assert.throws(function() {calculateNumber(NaN, 3), '[Function: TypeError]'});
