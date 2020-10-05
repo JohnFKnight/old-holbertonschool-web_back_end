eb10e1f8ef9177a36b03211a9c2fa299042e859f
jfkquality <jfkquality@gmail.com>
2020-10-05 03:38:58 +0000

    Task 0. Bufix. Change var name from calcNum to calculateNumber, the functions actual name.

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,29 +1,29 @@
 const assert = require ("assert");
-const calcNum = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calcNum', function () {
+describe('calculateNumber', function () {
   it('Add 2 integers', function () {
-    assert.strictEqual(calcNum(3, 4), 7);
+    assert.strictEqual(calculateNumber(3, 4), 7);
   });
   it('Add 2 floats', function () {
-    assert.strictEqual(calcNum(3.4, 4.2), 7);
+    assert.strictEqual(calculateNumber(3.4, 4.2), 7);
   });
   it('Add 2 floats .0', function () {
-    assert.strictEqual(calcNum(3.0, 4.0), 7);
+    assert.strictEqual(calculateNumber(3.0, 4.0), 7);
   });
   it('Add neg and pos ints; pos result', function () {
-    assert.strictEqual(calcNum(-3, 4), 1);
+    assert.strictEqual(calculateNumber(-3, 4), 1);
   });
   it('Add neg and pos ints; neg result', function () {
-    assert.strictEqual(calcNum(3, - 4), -1);
+    assert.strictEqual(calculateNumber(3, - 4), -1);
   });
   it('Add neg and pos flosts; pos result', function () {
-    assert.strictEqual(calcNum(-3.4, 4.5), 2);
+    assert.strictEqual(calculateNumber(-3.4, 4.5), 2);
   });
   it('Add neg and pos floats; neg result', function () {
-      assert.strictEqual(calcNum(-3.5, 2), -1);
+      assert.strictEqual(calculateNumber(-3.5, 2), -1);
   });
   it('should throw error if NaN passed', function () {
-      assert.throws(function () {calcNum(NaN, 3), '[Function: TypeError]'});
+      assert.throws(function () {calculateNumber(NaN, 3), '[Function: TypeError]'});
   });
 });
