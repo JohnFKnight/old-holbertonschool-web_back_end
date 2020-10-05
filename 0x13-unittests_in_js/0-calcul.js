7f156aad193e8813fa5f2df8c90dc71778738085
jfkquality <jfkquality@gmail.com>
2020-10-05 02:53:16 +0000

    Task 0. Bugfix: Change throw TypeError() to throw TypeError.

--- 0x13-unittests_in_js/0-calcul.js
+++ 0x13-unittests_in_js/0-calcul.js
@@ -1,12 +1,7 @@
 function calculateNumber(a, b) {
     if (isNaN(a) || isNaN(b)) {
-    throw TypeError();
+    throw TypeError;
   }
   return Math.round(a) + Math.round(b);
 };
 module.exports = calculateNumber;
-
-// console.log(calculateNumber(2, 3))
-// console.log(calculateNumber(1.2, 3.3));
-// console.log(calculateNumber(2.5, 3.4));
-// console.log(calculateNumber(10.2, 11.3));
