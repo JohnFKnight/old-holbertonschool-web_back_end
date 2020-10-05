2b697b0555da7f340b89f49125eba58594785d9c
jfkquality <jfkquality@gmail.com>
2020-10-05 02:33:40 +0000

    Task 0. Bugfix: Change equal to strictEqual.

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,30 +2,30 @@ const assert = require ("assert");
 const calcNum = require('./0-calcul.js');
 describe('Round', function () {
   it('Add 2 integers', function () {
-    assert.equal(calcNum(3, 4), 7);
+    assert.strictEqual(calcNum(3, 4), 7);
   });
   it('Add 2 floats', function () {
-    assert.equal(calcNum(3, 4), 7);
+    assert.strictEqual(calcNum(3, 4), 7);
   });
   it('Add 2 integers', function () {
-    assert.equal(calcNum(3, 4), 7);
+    assert.strictEqual(calcNum(3, 4), 7);
   });
   it('Add 2 integers', function () {
-    assert.equal(calcNum(3, 4), 7);
+    assert.strictEqual(calcNum(3, 4), 7);
   });
   it('Add 2 integers', function () {
-    assert.equal(calcNum(3, 4), 7);
+    assert.strictEqual(calcNum(3, 4), 7);
   });
   it('Add 2 integers', function () {
-    assert.equal(calcNum(3, 4), 7);
+    assert.strictEqual(calcNum(3, 4), 7);
   });
   it('Add 2 integers', function () {
-    assert.equal(calcNum(3, 4), 8);
+    assert.strictEqual(calcNum(3, 4), 8);
   });
   it('Add 2 integers', function () {
-    assert.equal(calcNum(3, 4), 7);
+    assert.strictEqual(calcNum(3, 4), 7);
   });
   it('Add 2 integers', function () {
-    assert.equal(calcNum(3, 4), 7);
+    assert.strictEqual(calcNum(3, 4), 7);
   });
 });
