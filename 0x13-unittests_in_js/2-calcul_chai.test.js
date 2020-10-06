8b3ead50fe8599d30af722a71cfbcd0969c3be6c
jfkquality <jfkquality@gmail.com>
2020-10-06 04:23:09 +0000

    Task 2. Bugfix: Remove multiple describes. Group TYPE  expects under separate it.

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -5,38 +5,34 @@ const should = chai.should();
 const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', function () {
-    describe('sum', function () {
-	it('should work without rounding', function () {
-	    expect(calculateNumber('SUM', 3, 4)).to.equal(7);
-	    expect(calculateNumber('SUM', 3.4, 4.2)).to.equal(7);
-	    expect(calculateNumber('SUM', 3.0, 4.0)).to.equal(7);
-	    expect(calculateNumber('SUM', -3, 4)).to.equal(1);
-	    expect(calculateNumber('SUM', 3, - 4)).to.equal(-1);
-	    expect(calculateNumber('SUM', -3.4, 4.5)).to.equal(2);
-	    expect(calculateNumber('SUM', -3.5, 2)).to.equal(-1);
-	});
+    it('Sum function with rounding', function () {
+	expect(calculateNumber('SUM', 3, 4)).to.equal(7);
+	expect(calculateNumber('SUM', 3.4, 4.2)).to.equal(7);
+	expect(calculateNumber('SUM', 3.0, 4.0)).to.equal(7);
+	expect(calculateNumber('SUM', -3, 4)).to.equal(1);
+	expect(calculateNumber('SUM', 3, - 4)).to.equal(-1);
+	expect(calculateNumber('SUM', -3.4, 4.5)).to.equal(2);
+	expect(calculateNumber('SUM', -3.5, 2)).to.equal(-1);
     });
-    describe('subtract', function () {
-	it('should work without rounding', function () {
-	    expect(calculateNumber('SUBTRACT', 3, 4)).to.equal(-1);
-	    expect(calculateNumber('SUBTRACT', 3.4, 4.2)).to.equal(-1);
-	    expect(calculateNumber('SUBTRACT', 3.0, 4.0)).to.equal(-1);
-	    expect(calculateNumber('SUBTRACT', -3, 4)).to.equal(-7);
-	    expect(calculateNumber('SUBTRACT', 3, -4)).to.equal(7);
-	    expect(calculateNumber('SUBTRACT', -3.4, 4.5)).to.equal(-8);
-	    expect(calculateNumber('SUBTRACT', -3.5, 2)).to.equal(-5);
-	});
+    it('Subtract function with rounding', function () {
+	expect(calculateNumber('SUBTRACT', 3, 4)).to.equal(-1);
+	expect(calculateNumber('SUBTRACT', 3.4, 4.2)).to.equal(-1);
+	expect(calculateNumber('SUBTRACT', 3.0, 4.0)).to.equal(-1);
+	expect(calculateNumber('SUBTRACT', -3, 4)).to.equal(-7);
+	expect(calculateNumber('SUBTRACT', 3, -4)).to.equal(7);
+	expect(calculateNumber('SUBTRACT', -3.4, 4.5)).to.equal(-8);
+	expect(calculateNumber('SUBTRACT', -3.5, 2)).to.equal(-5);
     });
-    describe('divide', function () {
-	it('should work without rounding', function () {
-	    expect(calculateNumber('DIVIDE', 3, 4)).to.equal(0.75);
-	    expect(calculateNumber('DIVIDE', 3.4, 4.2)).to.equal(0.75);
-	    expect(calculateNumber('DIVIDE', 3.0, 4.0)).to.equal(0.75);
-	    expect(calculateNumber('DIVIDE', -3, 4)).to.equal(-0.75);
-	    expect(calculateNumber('DIVIDE', 3, -4)).to.equal(-0.75);
-	    expect(calculateNumber('DIVIDE', -3.4, 4.5)).to.equal(-0.6);
-	    expect(calculateNumber('DIVIDE', -3.5, 2)).to.equal(-1.5);
-	    expect(calculateNumber('DIVIDE', -3.5, .4)).to.equal('Error');
-	});
+    it('Divide function with rounding', function () {
+	expect(calculateNumber('DIVIDE', 3, 4)).to.equal(0.75);
+	expect(calculateNumber('DIVIDE', 3.4, 4.2)).to.equal(0.75);
+	expect(calculateNumber('DIVIDE', 3.0, 4.0)).to.equal(0.75);
+	expect(calculateNumber('DIVIDE', -3, 4)).to.equal(-0.75);
+	expect(calculateNumber('DIVIDE', 3, -4)).to.equal(-0.75);
+	expect(calculateNumber('DIVIDE', -3.4, 4.5)).to.equal(-0.6);
+	expect(calculateNumber('DIVIDE', -3.5, 2)).to.equal(-1.5);
+    });
+    it('Divide by zero funtion. Return Error', function () {
+	expect(calculateNumber('DIVIDE', -3.5, .4)).to.equal('Error');
     });
 })
