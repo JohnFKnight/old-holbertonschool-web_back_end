1b834090d1f6a0338c5e578d71f567e4a1348a8c
jfkquality <jfkquality@gmail.com>
2020-10-06 03:28:15 +0000

    Task 2. Bugfix: Refactor to use Chai suite style: all expects in a suite under one it.

--- 0x13-unittests_in_js/2-calcul_chai.test.jsv1.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.jsv1.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
-const calculateNumber = require('./2-calcul.js');
+const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', function () {
     describe('sum', function () {
