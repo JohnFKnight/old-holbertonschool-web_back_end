5dbab24cbb9414d8bda73847ebfcf2061a5e891f
jfkquality <jfkquality@gmail.com>
2020-10-09 00:21:45 +0000

    614, Task 3. Bugfix: Use spy/callback as the function with the params!

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -9,11 +9,18 @@ const calcNum = require('./utils.js');
 
 describe('sendPayment', function () {
     it('should pass using sendpmt function', function () {
-	const callback = sinon.spy(calcNum.calculateNumber);
-	sendpmt(100, 20);
-	expect(callback.called);
+	// const callback = sinon.spy(calcNum.calculateNumber);
+	const callback = sinon.spy(sendpmt);
+	callback(100, 20);
+	// console.log(callback('SUM', 50, 37));
+	// console.log(callback.called);
+	// console.log(callback.getCall(0));
+	// console.log(callback.getCall(0).args);
+	// console.log(callback.getCall(0).returnValue);
+	assert(callback.called, "callback called");
        
     });
+
     // it('should not use Utils.calculateNumber function', function () {
     // 	const spy = sinon.spy(sendpmt);
     // 	calcNum.calculateNumber('SUM', 100, 20), spy;
